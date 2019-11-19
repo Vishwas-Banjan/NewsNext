c03e37adc0e40c6798dbca06923beaa2831add36
Vishwas Banjan <vishwasbanjan@gmail.com>
2019-11-19 18:19:24 -0500

    UI Update

--- app/src/main/java/com/vbanjan/newsnext/TopHeadlinesFragment.kt
+++ app/src/main/java/com/vbanjan/newsnext/TopHeadlinesFragment.kt
@@ -24,22 +24,23 @@ class TopHeadlinesFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         selectedSource = arguments!!.getSerializable("selectedSource").toString()
-        var sourceObj=arguments!!.getSerializable("selectedSource") as Source
+        var sourceObj = arguments!!.getSerializable("selectedSource") as Source
         GetTopHeadlines().execute(sourceObj.getID());
-        Log.d("demo",selectedSource)
     }
-    inner class GetTopHeadlines: AsyncTask<String, Void, ArrayList<HeadLine>>() {
+
+    inner class GetTopHeadlines : AsyncTask<String, Void, ArrayList<HeadLine>>() {
         override fun doInBackground(vararg p0: String): ArrayList<HeadLine> {
             var source = p0[0]
             var headlines = ArrayList<HeadLine>();
             var result = ""
             val client = OkHttpClient()
-            val url ="https://newsapi.org/v2/top-headlines?sources="+source+"&apiKey=2c5d920262c643b5889cd05d58a1ae53"
+            val url =
+                "https://newsapi.org/v2/top-headlines?sources=" + source + "&apiKey=2c5d920262c643b5889cd05d58a1ae53"
             val request: Request = Request.Builder().url(url).build()
             val response: Response = client.newCall(request).execute()
             if (!response.isSuccessful) throw IOException("Unexpected Code: $response")
             val json: String = response.body!!.string()
-            if(!json.isNullOrEmpty()) {
+            if (!json.isNullOrEmpty()) {
                 val jsonObject = JSONObject(json);
                 val jsonArray = jsonObject.getJSONArray("articles");
                 for (i in 0 until jsonArray.length()) {
@@ -47,15 +48,15 @@ class TopHeadlinesFragment : Fragment() {
                     val title = article.get("title").toString();
                     val description = article.get("description").toString();
                     val urlHL = article.get("url").toString();
-                    var urlTL=""
-                    if(!article.get("urlToImage").toString().equals("null")) {
+                    var urlTL = ""
+                    if (!article.get("urlToImage").toString().equals("null")) {
                         urlTL = article.get("urlToImage").toString();
-                    }else{
-                        urlTL=""
+                    } else {
+                        urlTL = ""
                     }
                     val publishedAt = article.get("publishedAt").toString();
-                    val content= article.get("content").toString();
-                    headlines.add(HeadLine(title, description, urlHL, urlTL,publishedAt,content))
+                    val content = article.get("content").toString();
+                    headlines.add(HeadLine(title, description, urlHL, urlTL, publishedAt, content))
                 }
             }
             return headlines;
@@ -63,12 +64,16 @@ class TopHeadlinesFragment : Fragment() {
 
         override fun onPostExecute(result: ArrayList<HeadLine>) {
             super.onPostExecute(result)
-            topHeadlinesRecyclerView.layoutManager=LinearLayoutManager(context)
-            topHeadlinesRecyclerView.adapter= HeadLineAdapter(result,context as Context)
+            topHeadlinesRecyclerView.layoutManager = LinearLayoutManager(context)
+            topHeadlinesRecyclerView.adapter = HeadLineAdapter(result, context as Context)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_top_headlines, container, false)
     }
 
