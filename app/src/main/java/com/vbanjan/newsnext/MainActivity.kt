96ad9197a74e7cb59995804c46c5961dbee87f51
Vishwas Banjan <vishwasbanjan@gmail.com>
2019-11-18 22:45:07 -0500

    Navgraph and Fragments Setup

--- app/src/main/java/com/vbanjan/newsnext/MainActivity.kt
+++ app/src/main/java/com/vbanjan/newsnext/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.vbanjan.newsnext
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SourcesFragment.OnFragmentInteractionListener,
+    TopHeadlinesFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
     }
 }
